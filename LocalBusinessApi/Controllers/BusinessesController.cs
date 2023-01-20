3c0eadfe4ad1c2b5c44e5d3b62876eba8b1951aa
Henry Sullivan <sullivanbhenry@gmail.com>
2023-01-20 10:58:48 -0800

    add U functionality to API

--- LocalBusinessApi/Controllers/BusinessesController.cs
+++ LocalBusinessApi/Controllers/BusinessesController.cs
@@ -44,5 +44,40 @@ namespace LocalBusinessApi.Controllers
       await _db.SaveChangesAsync();
       return CreatedAtAction(nameof(GetBusiness), new { id = business.BusinessId }, business);
     }
+
+    //PUT: api/businesses/{id}
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Business business)
+    {
+      if (id != business.BusinessId)
+      {
+        return BadRequest();
+      }
+
+      _db.Businesses.Update(business);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!BusinessExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool BusinessExists(int id)
+    {
+      return _db.Businesses.Any(e => e.BusinessId == id);
+    }
   }
 }
