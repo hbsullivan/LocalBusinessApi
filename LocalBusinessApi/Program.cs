63852ba568eb8ff7eb4daa23b16b150d49e19c88
Henry Sullivan <sullivanbhenry@gmail.com>
2023-01-20 13:55:27 -0800

    WIP: adding versioning to project

--- LocalBusinessApi/Program.cs
+++ LocalBusinessApi/Program.cs
@@ -1,5 +1,7 @@
 using LocalBusinessApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,15 +26,32 @@ builder.Services.AddApiVersioning(opt =>
                                                                                         new MediaTypeApiVersionReader("x-api-version"));
                                     });
 
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+    setup.GroupNameFormat = "'v'VVV";
+    setup.SubstituteApiVersionInUrl = true;
+});
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions.Reverse())
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                description.GroupName.ToUpperInvariant());
+        }
+    });
 }
 else
 {
